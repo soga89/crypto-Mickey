37b457eb9a799ec85b74b727118136341ef8844b
Aldo Silvestre <sogarapida@gmail.com>
2019-07-03 00:44:11 -0300

    modificaciones

--- codigo/cripto-mickey/src/mickey-2.c
+++ codigo/cripto-mickey/src/mickey-2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+//#include <sys/mman.h>
 #include <string.h>
 
 #include "mickey.h"
@@ -22,6 +22,76 @@ void strip_ext(char *fname)
 int main(int argc, char** argv) {
 
 	CONF conf;
+	char *filename = argv[1];
+
+
+	unsigned char key[10] = { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x12, 0x34 };
+	unsigned char iv[10] = { 0x9c, 0x53, 0x2f, 0x8a, 0xc3, 0xea, 0x4b, 0x2e, 0xa0, 0xf5 };
+
+	init_regs();
+
+	setear_key(&conf, key, 80, 32);
+
+	FILE *exein, *exeout;
+
+	exein = fopen(filename, "rb");
+	if (exein == NULL) {
+	    perror("error al abrir el archivo");
+	    return 0;
+	}
+
+	char modify_filename[100];
+	strcpy(modify_filename, filename);
+	strip_ext(modify_filename);
+	strcat(modify_filename, "_mod.bmp");
+
+	exeout = fopen(modify_filename, "wb");
+	if (exeout == NULL) {
+	    perror("error al crear o abrir el archivo a modificar");
+	    return 0;
+	}
+
+	setear_iv(conf, iv);
+
+	unsigned char* copiar(unsigned char* m){
+		unsigned char *a;
+			for (int j = 0; j < 8; j++)
+				a = ((int) a) ^ CLOCK_KG(conf, 0, 0) << (7 - j);
+		return a;
+	}
+
+	fseek(exein, 0L, SEEK_END);
+	size_t tama = ftell(exein);
+
+	fseek(exein, 0L, SEEK_SET);
+	rewind(exein);
+
+	size_t n, m;
+	unsigned char buff[tama];
+	unsigned char* aux = malloc(tama);
+
+    fread(buff, 1, 54, exein);
+    fwrite(buff,1,54, exeout);
+
+	do {
+	    n = fread(buff, 1, sizeof buff - 54, exein);
+
+	    if (n) {
+	    		encrypt_desencrypt_data(&conf, &iv, buff, aux, tama);
+	    		m = fwrite(aux, 1, n, exeout);
+	    }
+	    else   m = 0;
+
+	} while ((n > 0) && (n == m));
+	if (m) perror("copy");
+
+
+	if (fclose(exeout)) perror("error al cerrar el archivo");
+	if (fclose(exein)) perror("error al cerrar el archivo");
+
+	puts("Se proceso correctamente");
+
+	/*CONF conf;
 	char *filename = argv[1];
 	int file_origen, file_destino;
 	char *puntero_origen, *puntero_destino, *aux_o, *aux_d;
@@ -74,6 +144,6 @@ int main(int argc, char** argv) {
 	close(file_origen);
 	close(file_destino);
 	printf("Proceso realizado.\n");
-
+*/
 	return 0;
 }
